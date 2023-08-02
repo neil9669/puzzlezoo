1b894f91d9bd964b4f11a9322890e33e89774eb9
neil9669 <palmern69@gmail.com>
2023-08-02 20:48:35 +0000

    remove old files that were moved to assets

--- scripts/tests/puzzle.test.js
+++
@@ -1,8 +0,0 @@
-beforeAll(() => {
-    let fs = require("fs");
-    let fileContents = fs.readFileSync("index.html", "utf-8");
-    document.open();
-    document.write(fileContents);
-    document.close();
-});
-
