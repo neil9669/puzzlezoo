a81d3aafb463fcf826edec16b4b68935f405c7b7
neil9669 <palmern69@gmail.com>
2023-08-08 10:30:53 +0000

    add routine to shuffle puzzle pieces

--- assets/scripts/puzzle.js
+++ assets/scripts/puzzle.js
@@ -23,7 +23,19 @@ function placePiece(item, index) {
     document.getElementById("pieces-area").innerHTML += '<div class="col-3 puzzle-space">' + item + '</div>';
 }
 
+// Function to shuffle array (of pieces)
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
 
+// Shuffle the pieces
+shuffle(pieces);
 
 
 
