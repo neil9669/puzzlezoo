d405a0fcb8ef0f10ac95a52d1cebbe1e6f087118
neil9669 <palmern69@gmail.com>
2023-08-16 22:56:51 +0000

    add debug code and fix puzzles to swap and display correctly

--- assets/scripts/puzzle.js
+++ assets/scripts/puzzle.js
@@ -1,22 +1,24 @@
 // images for current puzzle
 var pieces = [
-    '<img src="../../assets/images/tiger/images/img1.png" alt="" data-piece="0" class="puzzle-piece" />',
-    '<img src="../../assets/images/tiger/images/img2.png" alt="" data-piece="1" class="puzzle-piece" />',
-    '<img src="../../assets/images/tiger/images/img3.png" alt="" data-piece="2" class="puzzle-piece" />',
-    '<img src="../../assets/images/tiger/images/img4.png" alt="" data-piece="3" class="puzzle-piece" />',
-    '<img src="../../assets/images/tiger/images/img5.png" alt="" data-piece="4" class="puzzle-piece" />',
-    '<img src="../../assets/images/tiger/images/img6.png" alt="" data-piece="5" class="puzzle-piece" />',
-    '<img src="../../assets/images/tiger/images/img7.png" alt="" data-piece="6" class="puzzle-piece" />',
-    '<img src="../../assets/images/tiger/images/img8.png" alt="" data-piece="7" class="puzzle-piece" />',
-    '<img src="../../assets/images/tiger/images/img9.png" alt="" data-piece="8" class="puzzle-piece" />',
-    '<img src="../../assets/images/tiger/images/img10.png" alt="" data-piece="9" class="puzzle-piece" />',
-    '<img src="../../assets/images/tiger/images/img11.png" alt="" data-piece="10" class="puzzle-piece" />',
-    '<img src="../../assets/images/tiger/images/img12.png" alt="" data-piece="11" class="puzzle-piece" />',
-    '<img src="../../assets/images/tiger/images/img13.png" alt="" data-piece="12" class="puzzle-piece" />',
-    '<img src="../../assets/images/tiger/images/img14.png" alt="" data-piece="13" class="puzzle-piece" />',
-    '<img src="../../assets/images/tiger/images/img15.png" alt="" data-piece="14" class="puzzle-piece" />',
-    '<img src="../../assets/images/tiger/images/img16.png" alt="" data-piece="15" class="puzzle-piece" />'
+    '<img src="../../assets/images/tiger/img1.png" alt="" data-piece="0" class="puzzle-piece" />',
+    '<img src="../../assets/images/tiger/img2.png" alt="" data-piece="1" class="puzzle-piece" />',
+    '<img src="../../assets/images/tiger/img3.png" alt="" data-piece="2" class="puzzle-piece" />',
+    '<img src="../../assets/images/tiger/img4.png" alt="" data-piece="3" class="puzzle-piece" />',
+    '<img src="../../assets/images/tiger/img5.png" alt="" data-piece="4" class="puzzle-piece" />',
+    '<img src="../../assets/images/tiger/img6.png" alt="" data-piece="5" class="puzzle-piece" />',
+    '<img src="../../assets/images/tiger/img7.png" alt="" data-piece="6" class="puzzle-piece" />',
+    '<img src="../../assets/images/tiger/img8.png" alt="" data-piece="7" class="puzzle-piece" />',
+    '<img src="../../assets/images/tiger/img9.png" alt="" data-piece="8" class="puzzle-piece" />',
+    '<img src="../../assets/images/tiger/img10.png" alt="" data-piece="9" class="puzzle-piece" />',
+    '<img src="../../assets/images/tiger/img11.png" alt="" data-piece="10" class="puzzle-piece" />',
+    '<img src="../../assets/images/tiger/img12.png" alt="" data-piece="11" class="puzzle-piece" />',
+    '<img src="../../assets/images/tiger/img13.png" alt="" data-piece="12" class="puzzle-piece" />',
+    '<img src="../../assets/images/tiger/img14.png" alt="" data-piece="13" class="puzzle-piece" />',
+    '<img src="../../assets/images/tiger/img15.png" alt="" data-piece="14" class="puzzle-piece" />',
+    '<img src="../../assets/images/tiger/img16.png" alt="" data-piece="15" class="puzzle-piece" />'
 ];
+
+
 
 // Function to shuffle array (of pieces)
 function shuffle(a) {
@@ -31,7 +33,12 @@ function shuffle(a) {
 
 // Function to place piece in to Div
 function placePiece(item, index) {
+    //    let test = document.getElementById("pieces-area").innerHTML += '<div class="col-3 puzzle-space">' + item + '</div>';
+
+
     document.getElementById("pieces-area").innerHTML += '<div class="col-3 puzzle-space">' + item + '</div>';
+    //console.log(test);
+    //  console.log(item, index);
 }
 
 // Shuffle the pieces
@@ -39,12 +46,16 @@ shuffle(pieces);
 
 // Place the pieces
 pieces.forEach(placePiece);
-
 // Puzzle status - trueif piece is in the correct place, otherwise false.
 var puzzleStatus = Array(pieces.length).fill(false);
 
+//function puzzleLoad();
+
 // Wait for website to load then run main jQuery functions
-$(document).ready(() => {
+// When document is ready...
+
+//$(document).ready(function() {
+function puzzleReady(jQuery) {
     // Make pieces draggable
     $(".puzzle-piece").draggable({
         //snap to grid
@@ -54,7 +65,7 @@ $(document).ready(() => {
         start: function (event, ui) {
             puzzleStatus[$(this).data("piece")] = false;
         }
-    });
+    }); // puzzle-piece end
 
     // Make board droppable
     $(".puzzle-drop-place").droppable({
@@ -72,13 +83,13 @@ $(document).ready(() => {
                 puzzleComplete();
             }
         }
-    });
+    }); //puzzle-drop-place end
 
-    // test the finished puzzle
-    //pieces.forEach(placePiece);
-    // $("#puzzle-complete").show();
 
-});
+    //}); // ready function end
+}
+
+$(document).ready(puzzleReady);
 
 // click event function to display instructions
 $('#instructions').click(function () {
@@ -86,7 +97,7 @@ $('#instructions').click(function () {
     $('#instructions-area').show();
     $('#board').css('display', 'flex');
     $('#pieces-area').css('display', 'none');
-});
+});  // instructions end
 
 // click event function to start the puzzle
 $('#start').click(function () {
@@ -94,52 +105,86 @@ $('#start').click(function () {
     $('#instructions-area').hide();
     $('#pieces-area').css('display', 'flex');
     $('#board').css('display', 'flex');
-});
+}); //Start end
 
 // click event function for puzzle choice
 $('#puzzle-choice').click(function () {
     $("#Puzzle-choices").show();
-    // $("#pieces-area").hide();
-    // $("#board").slideUp();
-    //$("#puzzle-complete").show().delay(1500).fadeOut();
-    // $("#puzzle-complete").show();
-    // $('#instructions-area').hide();
-    // $('#puzzle-complete-message').show();
-    // $("#puzzle-complete").fadeOut("slow", function () {
-    //   $("#puzzle-complete").load(function () { //avoiding blinking, wait until loaded
-    //       $("#family-pic").fadeIn();
-    //   });
-
-    //  });
-    // $("#family-pic").delay(1800).fadeIn(2000);
-
-
 });
 
 // click event function for elephant puzzle choice
 $('#elephant').click(function () {
-    alert('This is the elepahant function');
+    $("#Puzzle-choices").hide();
+    $("#pieces-area div").remove();
+    currPuzzle("elephant");
 });
 
 // click event function for monkey puzzle choice
 $('#monkey').click(function () {
-    alert('This is the monkey function');
+    $("#Puzzle-choices").hide();
+    $("#pieces-area div").remove();
+    currPuzzle("monkey");
+    $(onPageLoad);
 });
 
 // click event function for panda puzzle choice
 $('#panda').click(function () {
-    alert('This is the panda function');
+    $("#Puzzle-choices").hide();
+    $("#pieces-area div").remove();
+    currPuzzle("panda");
+    $(onPageLoad);
 });
 
 // click event function for tiger puzzle choice
 $('#tiger').click(function () {
-    alert('This is the tiger function');
+    $("#Puzzle-choices").hide();
+    $("#pieces-area div").remove();
+    currPuzzle("tiger");
+    $(onPageLoad);
 });
 
 // click event function for turtle puzzle choice
 $('#turtle').click(function () {
-    alert('This is the turtle function');
+    $("#Puzzle-choices").hide();
+    $("#pieces-area div").remove();
+    currPuzzle("turtle");
+    $(onPageLoad);
 });
+
+// click event function for debug choice
+$('#debug').click(function () {
+    console.log($("#board div"));
+    console.log($("#pieces-area div"));
+});
+
+// change puzzle based on choice
+function currPuzzle(puzzle) {
+    // Get name of old puzzle;
+    let temp = pieces[0].split('/', 5);
+    let oldPuzzle = temp[temp.length - 1];
+    // Replace the old puzzle divs with new puzzle
+    let puzzleDiv = $("#board div");
+    let puzzleVar = pieces[0];
+    let oldsrc = puzzleDiv[0].innerHTML;
+    let newsrc = oldsrc;
+
+    // change puzzle pieces location in pieces variable
+    for (let i = 0; i < pieces.length; ++i) {
+        puzzleVar = `<img src="../../assets/images/${puzzle}/img${i + 1}.png" alt="" data-piece="${i}" class="puzzle-piece" />`;
+        pieces[i] = puzzleVar;
+    }
+
+    // change puzzle pieces location in Divs in index.html 
+    for (let i = 0; i < puzzleDiv.length; ++i) {
+        newsrc = puzzleDiv[i].innerHTML.replace(oldPuzzle, puzzle);
+        puzzleDiv[i].innerHTML = newsrc;
+    }
+    $(puzzleReady);
+    // Shuffle the pieces
+    $(shuffle(pieces));
+    // Place the pieces
+    pieces.forEach(placePiece);
+} // currPuzzle end
 
 
 // Call this function when puzzle correctly completed
